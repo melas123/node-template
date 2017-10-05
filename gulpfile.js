b9a2cca51e1507f95942676b17261abd5e313e44
melas123 <salemharrathi@gmail.com>
2017-10-06 00:32:04 +0100

    configure basic gulp

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp    = require('gulp')
+var jshint    = require('gulp-jshint')
+
+var jsFiles = ["*.js", "src/**/*js"];
+gulp.task('style', function(){
+  return gulp.src(jsFiles)
+            .pipes(jshint())
+});
