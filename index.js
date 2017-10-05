5691707bf28fbdf72953f82df77cdc55ca82cae6
melas123 <salemharrathi@gmail.com>
2017-10-05 23:29:00 +0100

    Add bootstrap admin template

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var express = require('express')
 var app     = express()
 var server  = app.listen(3000);
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
