a0c7d569d20c782b5de4cef2afc74f06767ba701
melas123 <salemharrathi@gmail.com>
2017-10-05 23:23:39 +0100

    Add express

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-var colors = require('colors');
-console.log('Hello,'.red, 'npm!'.green);
+// var colors  = require('colors');
+var express = require('express')
+var app     = express()
+var server  = app.listen(3000);
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+// console.log("Express server listening on port 3000");
