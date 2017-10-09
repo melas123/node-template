6471d78efd91df506f6a75f12609b0cb2b486bb9
melas123 <salemharrathi@gmail.com>
2017-10-09 13:18:56 +0100

    Configure EJS template engine

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var app     = express()
 var server  = app.listen(3000);
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
+app.get('/', function (req, res) {
+    res.render('index',{list:  ['val1', 'val2', 'val3']});
+})
+app.get('/tables', function (req, res) {
+    res.render('pages/tables');
+});
 // console.log("Express server listening on port 3000");
